7bc3ffc451eb2c11a6f6621113082b850cb4d0c3
Hunterxia <hunterxia@outlook.com>
2024-02-15 10:05:00 -0600

    Update next.config.mjs and expert page layout

--- app/experts/[id]/page.jsx
+++ app/experts/[id]/page.jsx
@@ -1,38 +1,37 @@
+"use client";
 import experts from "../../data/experts_data.json";
+import { Button } from "@/components/ui/button";
 
 export default function Page({ params }) {
   const expert = experts.find((expert) => expert.id === params.id);
 
+  const handleContactClick = () => {
+    window.location.href = `mailto:${expert.email}`;
+  };
+
   return (
     <div className="container mx-auto p-4">
       {expert ? (
-        <div className=" mx-auto">
-          <div className="">
-            {/* Image container */}
-            <div className=" overflow-hidden">
-              <img
-                src={expert.image_src}
-                alt={expert.name}
-                className=" h-auto" // This ensures the image is fully visible and responsive
-              />
-            </div>
+        <div className="mx-auto">
+          <div className="overflow-hidden">
+            <img src={expert.image_src} alt={expert.name} className="h-auto" />
+          </div>
+          <div className="bg-white p-4 shadow-lg rounded-lg mt-4">
+            <h1 className="text-2xl font-bold mb-2">{expert.name}</h1>
+            <p className="mb-2">
+              <strong>Email:</strong> {expert.email}
+            </p>
+            <p className="mb-2">
+              <strong>School:</strong> {expert.school}
+            </p>
+            <p className="mb-2">
+              <strong>Title:</strong> {expert.title}
+            </p>
+            <p className="mb-2">
+              <strong>Focus Areas:</strong> {expert.focus_areas.join(", ")}
+            </p>
 
-            {/* Expert details */}
-            <div className="bg-white p-4 shadow-lg rounded-lg mt-4">
-              <h1 className="text-2xl font-bold mb-2">{expert.name}</h1>
-              <p className="mb-2">
-                <strong>Email:</strong> {expert.email}
-              </p>
-              <p className="mb-2">
-                <strong>School:</strong> {expert.school}
-              </p>
-              <p className="mb-2">
-                <strong>Title:</strong> {expert.title}
-              </p>
-              <p className="mb-2">
-                <strong>Focus Areas:</strong> {expert.focus_areas.join(", ")}
-              </p>
-            </div>
+            <Button onClick={handleContactClick}>Contact {expert.name}</Button>
           </div>
         </div>
       ) : (
